03300f296830050d9e50cac426b18378d79539c8
Krityuk Kumar <krityuk@example.com>
2025-06-10 18:26:13 +0530

    quantum-website-continued

--- Zeus Task 3/script.js
+++ Zeus Task 3/script.js
@@ -1,4 +1,5 @@
 function displayFactorial() {
+    console.time("Execution Time");
     const num = document.getElementById('numberInput').value;
     const result = document.getElementById('result'); // result var is having ref, its not local variable so no need to use setState
     if(!num){
@@ -15,11 +16,14 @@ function displayFactorial() {
         factorial = multiplyNumbers(factorial, i);
     }
     
-    result.textContent = `factorial of ${num} is ${factorial.join('')}`;
-    
+    result.textContent = `factorial of ${num} is ${factorial.join(' ')}`;
+    console.timeEnd("Execution Time");
+    alert(`Time taken is ${performance.now().toFixed(2)} ms`);
+
 }
 
 document.getElementById('submitButton').addEventListener('click', displayFactorial);
+
 
 
 function multiplyNumbers(arr,b) {
@@ -27,14 +31,16 @@ function multiplyNumbers(arr,b) {
     // arr = [5,0,4,0,3,0,0,0,0] b = 23
     let carry = 0;
     let result = [];
-    for(let i = arr.length - 1; i >= 0; i--) {
-        let product = arr[i] * b + carry;
-        result.unshift(product % 10); // get the last digit
-        carry = Math.floor(product / 10); // get the carry
+    for(let i = arr.length - 1; i >= 0; i--) {     // 5040 3000 5340 0000 * 23
+        let product = arr[i] * b + carry; 
+        // NOTE HERE YOU SHOULD TAKE AS BIG RADIX AS POSSIBLE, BUT MAKE SURE arr[i] * b + carry not exceed 9e15 as it is javascript number limit 
+        // if we keep radix = 10000000, then max value of arr[i] is 9999999.
+        result.unshift(product % 10000000);
+        carry = Math.floor(product / 10000000); // get the carry
     }
     while(carry>0){
-        result.unshift(carry % 10); // get the last digit of carry
-        carry = Math.floor(carry / 10); // update carry
+        result.unshift(carry % 10000000); // getting the last digit of carry
+        carry = Math.floor(carry / 10000000); // updating carry
     }
    return result;
 }
