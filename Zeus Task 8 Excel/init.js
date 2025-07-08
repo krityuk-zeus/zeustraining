a847844304e18d15282a096650c7fb75fc26335b
Krityuk Kumar <krityuk@example.com>
2025-07-08 16:19:29 +0530

    minor changes

--- Zeus Task 8 Excel/init.js
+++ Zeus Task 8 Excel/init.js
@@ -4,13 +4,13 @@ import loadDefaultData from './services/load_default_data.js';
 
 /**
  * Initializes the Excel-like UI:
- * - Creates and appends the header with an Upload button.
+ * - Creates a header object and appends it into body.
  * - Loads default data to initialize the grid.
  * NOTE : loadDefaultData function would call the constructor of grid,
  * and grid-constructor is having the canvas, header, and sidebar etc injected into the container.
  */
 export default function initializeApp() {
   const header = createExcelHeader(handleUpload);
-  document.body.prepend(header);
+  document.body.prepend(header); // Conatainer is already injected into body in index.html
   loadDefaultData();
 }
--- Zeus Task 8 Excel/main.js
+++ Zeus Task 8 Excel/main.js
@@ -1,3 +1,7 @@
 import initializeApp from './init.js';
 
+/**
+ * Initializes the application by setting up the UI and loading default data.
+ * This function is the entry point for the application.
+ */
 initializeApp();
